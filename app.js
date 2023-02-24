5555ba017b728bdf766a600ec4dfedca5d555ca0
Daniel Dip <dani.dip.2@gmail.com>
2023-02-24 14:48:53 -0300

    bug cerdo

--- app.js
+++ app.js
@@ -1,107 +1,258 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
-const {getCerdo, getListado, getOfertas, getPescado, getPollo, getVaca, getOfertasNames} = require('./functions.js')
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const JsonFileAdapter = require('@bot-whatsapp/database/json')
+const {
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
+} = require("@bot-whatsapp/bot");
+const {
+  getCerdo,
+  getListado,
+  getOfertas,
+  getPescado,
+  getPollo,
+  getVaca,
+  getOfertasNames,
+  getCerdoMsg,
+} = require("./functions.js");
+// const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require("@bot-whatsapp/provider/baileys");
+const JsonFileAdapter = require("@bot-whatsapp/database/json");
+const mySqlAdapter = require("@bot-whatsapp/database/mysql");
 
-let total=0
+let STATE_APP = { total: 0 };
+let parcial = 0;
+let productoBuscado = { name: "", price: 0 };
 
-const flowEfectivo = addKeyword(['efectivo']).addAnswer('Lo espero con los billetes!😃') 
-const flowOnline = addKeyword(['online']).addAnswer(`Espero su comprobante! El monto es: ${total}, este es el link https://mpago.la/16AGQBt`)
 
-const flowPago = addKeyword(['pago', 'pagar', 'ir a pagar']).addAnswer(
-    'por que medio realizara el pago?', 
-    {
-        buttons: [{ body: 'efectivo' }, { body: 'online' }]
-    },
-    null,
-    [flowEfectivo, flowOnline]
-)
+const flowCantidad = addKeyword(["genial"]).addAnswer(
+  [
+    `a continuacion indica cuantos kgs de ${productoBuscado.name} quiere solo caracteres numericos!:`,
+  ],
+  { capture: true },
+  async (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = productoBuscado.price * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return await flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
 
-const flowSeguirComprando = addKeyword(['seguir', 'seguir comprando']).addAnswer(
-    'que desea seguir viendo?',
-    {
-        buttons: [{ body: 'ofertas' }, { body: 'listado' }]
-    },
-    null,
-    [flowOfertas, flowListado]
-)
+const flowCerdo = addKeyword("cerdo").addAnswer(
+  [
+    "a continuacion indica el nombre del producto que le interesa:",
+    getCerdoMsg(),
+  ],
+  {capture:true},
+  async (ctx,{flowDynamic, fallBack}) => {
+    let productos = getCerdo();
+    productoBuscado = productos.filter(
+      (e) => e.name.toLowerCase() == ctx.body.toLowerCase()
+    );
+    if (!productoBuscado) return fallBack();
+    console.log(productoBuscado)
+     await flowDynamic('genial') 
+  }
+).addAnswer('asdadasdaddada', {delay:2500});
 
-const flowOferta1 = addKeyword(['Pata muslo']).addAnswer(
-    'a continuacion indica cuantas promociones quiere, solo caracteres numericos!:',
-    {capture:true},
-    (ctx)=>{
-        if(typeof ctx.body !== 'number') return fallBack()
-        total+= 1290*ctx.body
-        return `Genial! tu precio total es ${1290*ctx.body}`
-    },
-)
+const flowVaca = addKeyword(["vaca"]).addAnswer(
+  ["a continuacion indica el nombre del producto que le interesa:", getVaca()],
+  { capture: true },
+  (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 1399 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
 
-const flowOferta2 = addKeyword(['8 medallones vacunos']).addAnswer(
-    'a continuacion indica cuantas promociones quiere, solo caracteres numericos!:',
-    {capture:true},
-    (ctx)=>{
-        if(typeof ctx.body !== 'number') return fallBack()
-        total+= 849*ctx.body
-        return `Genial! tu precio total es ${1290*ctx.body}`
-    },
-).addAnswer('desea seguir comprando o ir a pagar?',)
+const flowPollo = addKeyword(["pollo"]).addAnswer(
+  ["a continuacion indica el nombre del producto que le interesa:", getPollo()],
+  { capture: true },
+  (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 1399 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
 
-const flowOferta3 = addKeyword(['Bondiola']).addAnswer(
-    'a continuacion indica cuantas promociones quiere, solo caracteres numericos!:',
-    {capture:true},
-    (ctx)=>{
-        if(typeof ctx.body !== 'number') return fallBack()
-        total+= 1399*ctx.body
-        return `Genial! tu precio total es ${1399*ctx.body}`
-    },
-)
+const flowPescado = addKeyword(["Pescado"]).addAnswer(
+  ["a continuacion indica el nombre del producto que le interesa:", getPescado],
+  { capture: true },
+  (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 1399 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
+const flowOferta1 = addKeyword(["Pata muslo"]).addAnswer(
+  "a continuacion indica cuantas promociones quiere, solo caracteres numericos!:",
+  { capture: true },
+  (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 1290 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
 
+const flowOferta2 = addKeyword(["8 medallones vacunos"]).addAnswer(
+  [
+    "a continuacion indica cuantas promociones quiere, solo caracteres numericos!:",
+  ],
+  { capture: true },
+  async (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 849 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
+
+const flowOferta3 = addKeyword(["Bondiola"]).addAnswer(
+  "a continuacion indica cuantas promociones quiere, solo caracteres numericos!:",
+  { capture: true },
+  (ctx, { fallBack, flowDynamic }) => {
+    let num = Number(ctx.body);
+    if (isNaN(num)) return fallBack();
+    parcial = 1399 * num;
+    STATE_APP[ctx.from] = {
+      ...STATE_APP[ctx.from],
+      total: (STATE_APP.total += parcial),
+    };
+    return flowDynamic(
+      `Genial! tu precio por este producto es ${parcial}, si desea continuar comprando escriba *continuar*, si quiere ir a pagar escriba *pagar*`
+    );
+  }
+);
+
+const flowOfertas = addKeyword(["pedir oferta", "oferta"]).addAnswer(
+  "selecciona el nombre de la oferta que te interesa:",
+  {
+    buttons: [
+      { body: "Pata muslo" },
+      { body: "8 medallones vacunos" },
+      { body: "bondiola" },
     ],
-    null,
-    null,
-    []
-)
+  },
+  null,
+  [flowOferta1, flowOferta2, flowOferta3]
+);
 
-const flowOfertas = addKeyword(['pedir oferta', 'oferta']).addAnswer(
-    'selecciona el nombre de la oferta que te interesa:', {
-        buttons: [{ body: 'Pata muslo' }, { body: '8 medallones vacunos' }, { body: 'bondiola' }]
+const flowListado = addKeyword(["listado"]).addAnswer(
+  [
+    "A continuacion escriba que producto le interesa? *Cerdo*, *Pollo*, *Vaca* o *Pescado*",
+  ],
+  null,
+  null,
+  [flowCerdo, flowVaca, flowPollo, flowPescado]
+);
+
+const flowEfectivo = addKeyword(["efectivo"]).addAnswer(
+  "Lo espero con los billetes!😃"
+);
+const flowOnline = addKeyword(["online"]).addAnswer(
+  `Espero su comprobante! `,
+  null,
+  async (ctx, { flowDynamic }) => {
+    flowDynamic(
+      `El monto es:$${
+        STATE_APP[ctx.from].total
+      } , este es el link https://mpago.la/16AGQBt`
+    );
+  }
+);
+
+const flowPago = addKeyword(["pago", "pagar", "ir a pagar"])
+  .addAnswer(["Tu total es:"], null, async (ctx, { flowDynamic }) => {
+    console.log(STATE_APP, ctx);
+    await flowDynamic(`$ ${STATE_APP[ctx.from].total}`);
+  })
+  .addAnswer(
+    `por que medio quiere pagar?`,
+    {
+      buttons: [{ body: "efectivo" }, { body: "online" }],
     },
-    null,
-    [flowOferta1, flowOferta2, flowOferta3]
-)
+    { delay: 2000 },
+    [flowEfectivo, flowOnline]
+  );
 
-const flowListado = addKeyword(['listado']).addAnswer(
-    [''],
-    null,
-    null,
-    []
-)
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a *Carnave*')
-    .addAnswer(
-        getOfertas(),
-        null,
-        null,
-        [flowOfertas, flowListado]
-    ).addAnswer("Si desea comprar una de las ofertas escriba *oferta*\nPara solicitar el listado de precios escriba : Listado",null,null,[flowListado, flowOfertas])
+const flowContinuar = addKeyword(["continuar"])
+  .addAnswer("🙌 Seleccione que quiere ver:")
+  .addAnswer(
+    "Si desea comprar una de las ofertas escriba *oferta*\nPara solicitar el listado de precios escriba : *Listado*",
+    null,
+    null,
+    [flowListado, flowOfertas]
+  );
+
+const flowPrincipal = addKeyword(["hola", "ole", "alo"])
+  .addAnswer("🙌 Hola bienvenido a *Carnave*")
+  .addAnswer(getOfertas(), null, null, [flowOfertas, flowListado])
+  .addAnswer(
+    "Si desea comprar una de las ofertas escriba *oferta*\nPara solicitar el listado de precios escriba : Listado",
+    null,
+    null,
+    [flowListado, flowOfertas]
+  );
 
 const main = async () => {
-    const adapterDB = new JsonFileAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
-    const adapterProvider = createProvider(BaileysProvider)
+  const adapterDB = new JsonFileAdapter();
+  const adapterFlow = createFlow([
+    flowPrincipal,
+    flowContinuar,
+    flowPago,
+    flowCantidad,
+  ]);
+  const adapterProvider = createProvider(BaileysProvider);
 
-    createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
 
-    QRPortalWeb()
-}
+  // QRPortalWeb()
+};
 
-main()
+main();
--- functions.js
+++ functions.js
@@ -1,48 +1,70 @@
-const productos = require('./stock.json')
+const productos = require("./stock.json");
 
- const getOfertas = () =>{
-    let ofertas = productos[0].Productos.ofertas
-    let msg = ofertas.map((e)=>{return `*${e.name}* el precio es: $${e.price}\n`})
-    return msg
-}
- const getOfertasNames = () =>{
-    let ofertas = productos[0].Productos.ofertas
-    let msg = ofertas.map((e)=>{return `*${e.name}*`})
-    return msg
-}
+const getOfertas = () => {
+  let ofertas = productos[0].Productos.ofertas;
+  let msg = ofertas.map((e) => {
+    return `*${e.name}* el precio es: $${e.price}\n`;
+  });
+  return msg;
+};
+const getOfertasNames = () => {
+  let ofertas = productos[0].Productos.ofertas;
+  let msg = ofertas.map((e) => {
+    return `*${e.name}*`;
+  });
+  return msg;
+};
 
- const getListado = () =>{
-    let listado = productos[0].Productos.listado
-    return listado
-}
+const getListado = () => {
+  let listado = productos[0].Productos.listado;
+  return listado;
+};
 
- const getPollo = ()=>{
-    let lista = getListado()
-    let pollo = lista.pollo.map((e)=>{return `*${e.name}* el precio es: $${e.price}\n`})
-    return pollo
-}
+const getPollo = () => {
+  let lista = getListado();
+  let pollo = lista.pollo.map((e) => {
+    return `*${e.name}* el precio es: $${e.price}\n`;
+  });
+  return pollo;
+};
 
- const getVaca = ()=>{
-    let lista = getListado()
-    let vaca = lista.vaca.map((e)=>{return `*${e.name}* el precio es: $${e.price}\n`})
-    return vaca
-}
+const getVaca = () => {
+  let lista = getListado();
+  let vaca = lista.vaca.map((e) => {
+    return `*${e.name}* el precio es: $${e.price}\n`;
+  });
+  return vaca;
+};
 
- const getCerdo = ()=>{
-    let lista = getListado()
-    let cerdo = lista.cerdo.map((e)=>{return `*${e.name}* el precio es: $${e.price}\n`})
-    return cerdo
-}
+const getCerdo = () => {
+  let lista = getListado();
+  let cerdo = lista.cerdo;
+  return cerdo;
+};
 
- const getPescado = ()=>{
-    let lista = getListado()
-    let pescado = lista.pescado.map((e)=>{return `*${e.name}* el precio es: $${e.price}\n`})
-    return pescado
-}
+const getCerdoMsg = () => {
+  let cerdo = getCerdo();
+  let msg = cerdo.map((e) => {
+    return `*${e.name}* el precio es: $${e.price}, por KILO\n`;
+  });
+  return msg;
+};
+
+const getPescado = () => {
+  let lista = getListado();
+  let pescado = lista.pescado.map((e) => {
+    return `*${e.name}* el precio es: $${e.price}\n`;
+  });
+  return pescado;
+};
 
 module.exports = {
-    getCerdo,
-    getListado,
-    getOfertas,
-    getOfertasNames, getPescado, getPollo, getVaca
-}
+  getCerdo,
+  getCerdoMsg,
+  getListado,
+  getOfertas,
+  getOfertasNames,
+  getPescado,
+  getPollo,
+  getVaca,
+};
